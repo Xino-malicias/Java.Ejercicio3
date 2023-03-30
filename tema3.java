4c4c3b2af09ef598ab5c2c16d82078043e37eeba
Xino-malicias <107042296+Xino-malicias@users.noreply.github.com>
2023-03-30 17:53:15 +0200

    Add files via upload

---
+++ tema3.java
@@ -0,0 +1,12 @@
+package com.example.Ejercicios.Java;
+
+public class tema3 {
+
+    public static void main(String[] args) {
+
+        String[] libros = {"El Paciente", "Reina Roja", "Loba Negra", "Rey Blanco"};
+        for (int i = 0; i < libros.length; i++) {
+            System.out.println("Mi libro favorito es " + libros [i]);
+        }
+    }
+}
